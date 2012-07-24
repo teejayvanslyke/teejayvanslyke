8079bb50846e4ff30b2e31905900225cb417e327
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2012-07-24 09:44:21 -0400

    Gorgeous tweaks. Looking sharp and hella adaptive baby

--- content/assets/js/application.js
+++ content/assets/js/application.js
@@ -4,5 +4,21 @@ $(function() {
     isFitWidth: true,
     columnWidth: 193
   });
+
+  $('.block-link').click(function() {
+    $(this).find('.block').css({
+      position: 'fixed',
+      zIndex: '100000'
+    }).animate({
+      top: '0px',
+      left: '0px',
+      width: '100%',
+      height: '100%'
+    }, function() {
+      $(this).find('.back').fadeIn();
+    });
+
+    return false;
+  });
 });
 
