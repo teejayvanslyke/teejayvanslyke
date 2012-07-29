2640f36840dab92507141bc44e9b34f97f4d8fc7
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2012-07-29 15:43:55 -0700

    First iteration of overlay viewport complete. Yay

--- content/assets/js/application.js
+++ content/assets/js/application.js
@@ -9,6 +9,29 @@ $(function() {
     });
   }
 
+  $('a[rel=viewer]').click(function() {
+    var href = $(this).attr('href');
+
+    $('.viewer .loader').show();
+    $('.viewer-content').html('').load(href, function() {
+      $('.viewer .loader').hide();
+    });
+
+    $('html, body').addClass('noscroll');
+
+    $('.viewer-overlay').fadeIn(100);
+    $('.viewer-container').fadeIn(100);
+
+    $('.viewer-content').click(function() { return false; });
+    $('.viewer-container').click(function() {
+      $('.viewer-container, .viewer-overlay').fadeOut(100);
+      $('html, body').removeClass('noscroll');
+    });
+
+    return false;
+  });
+
+
   $.getJSON("http://twitter.com/statuses/user_timeline/teejayvanslyke.json?callback=?", function(data) {
      $("#tweet .body p").html(data[0].text);
   });
