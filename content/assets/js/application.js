844043d3c4aa66d4971fe47514cacfd894cb2801
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2012-08-09 18:08:09 -0700

    History state updates now

--- content/assets/js/application.js
+++ content/assets/js/application.js
@@ -11,10 +11,11 @@ $(function() {
 
   $('a[rel=viewer]').click(function() {
     var href = $(this).attr('href');
+    history.pushState(null, null, href);
 
-    $('.viewer .loader').show();
-    $('.viewer-content').html('').load(href, function() {
-      $('.viewer .loader').hide();
+    $('.viewer-loader').show();
+    $('.viewer-container').html('').load(href, function() {
+      $('.viewer-loader').hide();
     });
 
     $('html, body').addClass('noscroll');
@@ -22,8 +23,9 @@ $(function() {
     $('.viewer-overlay').fadeIn(100);
     $('.viewer-container').fadeIn(100);
 
-    $('.viewer-content').click(function() { return false; });
+    $('.viewer-content').click(function(event) { event.stopImmediatePropagation(); return false; });
     $('.viewer-container').click(function() {
+      history.pushState(null, null, '/');
       $('.viewer-container, .viewer-overlay').fadeOut(100);
       $('html, body').removeClass('noscroll');
     });
